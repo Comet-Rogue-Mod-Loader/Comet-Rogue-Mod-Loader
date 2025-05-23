bbad232449e505c174ba153bf5d0bf8ff212c189
Wh4I3 <154924333+Wh4I3-2@users.noreply.github.com>
2025-05-23 06:58:44 +0200

    Delete Mod/ModLoaderCore directory

--- Mod/ModLoaderCore/mod.gd
+++
@@ -1,7 +0,0 @@
-extends Mod
-
-func id() -> String: return "mod_loader"
-func display_name() -> String: return "Mod Loader Core"
-func description() -> String: return "Core of the mod loader, may not be disabled"
-func version() -> String: return "v0.0.0"
-func credit() -> String: return "Wh4I3"
