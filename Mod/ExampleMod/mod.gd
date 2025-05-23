bc5ed7c53fc07a4400f9c1a463b98ebee33420c0
Wh4I3 <154924333+Wh4I3-2@users.noreply.github.com>
2025-05-23 06:59:24 +0200

    Delete Mod/ExampleMod directory

--- Mod/ExampleMod/mod.gd
+++
@@ -1,7 +0,0 @@
-extends Mod
-
-func id() -> String: return "example_mod"
-func display_name() -> String: return "Example Mod"
-func description() -> String: return "A simple mod to help get modders started"
-func version() -> String: return "v0.0.1"
-func credit() -> String: return "Wh4I3"
